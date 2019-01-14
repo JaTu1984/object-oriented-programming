a6074ba0f3a41c062d414edc51d67c942b2144b0
Janne M Tuomela <JaTu1984@gmail.com>
2019-01-14 08:20:38 +0200

    Auto-ohjelma created, coded, working and added.

---
+++ object-oriented-programming/OOP Harjoitukset/Auto-ohjelma/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto_ohjelma
+{
+    class Car
+    {
+        public string brand;
+        public int speed;
+        public int newSpeed;
+        public int brake;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string brand, int speed)
+        {
+            this.brand = brand;
+            this.speed = speed;
+        }
+        public void AskData()
+
+        {
+            Console.WriteLine("Anna auton merkki?");
+            this.brand = Console.ReadLine();
+            Console.WriteLine("Anna auton nopeus?");
+            this.speed = int.Parse(Console.ReadLine());
+        }
+
+        public string ShowCarInfo()
+
+        {
+            return "Auton merkki on " + this.brand +
+                  " ja auton nopeus on " + this.speed;
+        }
+
+        public int Accelerate()
+
+        {
+            Console.WriteLine("Syötä positiivinen nopeuden muutos");
+            newSpeed = this.speed + int.Parse(Console.ReadLine());
+            {
+                if (newSpeed < this.speed)
+                    Console.WriteLine("Negatiivinen muutos ei mahdollinen");
+                
+                else
+                    Console.WriteLine("Auton uusi nopeus on ");
+                    return newSpeed;
+            }
+
+        }
+
+        public int Brake()
+        {
+            brake = (speed / 10) * 9;
+            Console.WriteLine("Auton hidastettu nopeus on ");
+            return brake;
+        }
+
+    }
+}
---
+++ object-oriented-programming/OOP Harjoitukset/Auto-ohjelma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Auto_ohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on auto-ohjelma");
+
+            Car myCar = new Car();
+            myCar.AskData();
+            Console.WriteLine(myCar.ShowCarInfo());
+            Console.WriteLine(myCar.Accelerate());
+            Console.WriteLine(myCar.Brake());
+            Car secondCar = new Car();
+            secondCar.AskData();
+            Console.WriteLine(secondCar.ShowCarInfo());
+            
+
+        }
+    }
+}
