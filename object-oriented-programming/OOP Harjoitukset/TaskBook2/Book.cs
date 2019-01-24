c4ac18554505a0b0d90d1c31254f33b63a4019c5
Janne M Tuomela <JaTu1984@gmail.com>
2019-01-24 05:55:45 +0200

    Kirja- ja Kirjailija-luokka created, coded, done and working.

---
+++ object-oriented-programming/OOP Harjoitukset/TaskBook2/Book.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace TaskBook2
+
+{
+    class Book
+
+    {
+        private string _name;
+        private readonly string _author;
+        private string _publisher;
+        private double _price;
+        private static string _category;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+
+        public Book()
+
+        {
+            Name = "unknown";
+            _author = "unknown";
+            Publisher = "unknown";
+            Price = 0;
+            _category = "unknown";
+        }
+
+        public Book(string name, string author, string publisher, double price, string category)
+
+        {
+            Name = name;
+            _author = author;
+            Publisher = publisher;
+            Price = price;
+            _category = category;
+
+        }
+
+        /// <summary>
+        /// Encapsulated properties
+        /// </summary>
+
+        public string Name
+
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Author
+
+        {
+            get { return _author; }
+            //set { _author = value; }
+        }
+
+        public string Publisher
+
+        {
+            get { return _publisher; }
+            set { _publisher = value; }
+        }
+
+        public double Price
+
+        {
+            get { return _price; }
+            set
+
+            {
+                _price = value;
+                if (value > 30)
+                    _price = value * 0.9;
+            }
+        }
+
+        public static string Category
+
+        {
+            get { return _category; }
+            //set { _category = value; }
+        }
+
+        public void GetBook(string name)
+
+        {
+            if (Name == name)
+            {
+                Console.WriteLine($"Kirja löytyi!\n" +
+                                  $"---------------------\n" +
+                                  $"Kirja: {Name}\n" +
+                                  $"Kirjailija: {Author}\n" +
+                                  $"Julkaisija: {Publisher}\n" +
+                                  $"Hinta: {Price}\n" +
+                                  $"----------------------\n");
+            }
+            else
+            {
+                Console.WriteLine($"{name} kirjaa ei löydy!\n" +
+                                  $"----------------------\n");
+            }
+
+        }
+
+        public static void ChangeCategory(string category)
+
+        {
+            _category = category;
+        }
+
+        public override string ToString()
+
+        {
+            return ($"Kirja: {Name}\n" +
+                    $"Kirjailija: {Author}\n" +
+                    $"Julkaisija: {Publisher}\n" +
+                    $"Hinta: {Price}\n" +
+                    $"----------------------\n");
+        }
+    }
+}
+
---
+++ object-oriented-programming/OOP Harjoitukset/TaskBook2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Literature;
+
+namespace TaskBook2
+
+{
+    class Program
+        
+    {
+        static void Main(string[] args)
+
+        {
+            Book book1 = new Book("Häräntappo ase", "Anna-Leena Härkönen", "Otava", 10, "Draama");
+            Console.WriteLine("Book luokan testeri!");
+            Console.WriteLine($"Kategoria nyt: {Book.Category}");
+            Book.ChangeCategory("Huumori");
+            Console.WriteLine($"Kategoria nyt: {Book.Category}");
+            Book book2 = new Book("Tuntematon sotilas", "Väinö Linna", "Otava", 100, "Sota");
+            book1.GetBook("Tuntematon sotilas");
+            book2.GetBook("Tuntematon sotilas");
+            Console.WriteLine(book1.Author);
+            Author author = new Author("Väinö XXX", "12.12.1934", book2);
+            author.PrintData();
+            Author author2 = new Author("Pekka", "1.1.1990", new Book("Mielensä pahoittaja", "Pekka Pouta", "VSOY", 15, "Huumori"));
+            author2.PrintData();
+        }
+
+        public static Book[] CreateData(Book[] books)
+
+        {
+            books[0] = new Book("Häräntappo ase", "Anna-Leena Härkönen", "Otava", 10, "Draama");
+            books[1] = new Book("Tuntematon sotilas", "Väinö Linna", "Otava", 100, "Sota");
+            books[2] = new Book("Sinuhe egyptiläinen", "Mika Waltari", "Otava", 25, "Draama");
+            books[3] = new Book("Uhrilento", "Ilkka Remes", "VSOY", 25, "Jännitys");
+            books[4] = new Book("Mielensäpahoittaja", "", "Otava", 50, "Huumori");
+
+            return books;
+        }
+    }
+}
